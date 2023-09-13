5891b9d83796cadcde671d097bb088eafe4c584d
rodercode <rodersnow@gmail.com>
2023-09-13 15:09:49 +0200

    add service, repo and restcontroller annonations

--- src/main/java/com/example/petclinicdemo/Service/CustomerService.java
+++ src/main/java/com/example/petclinicdemo/Service/CustomerService.java
@@ -1,4 +1,7 @@
 package com.example.petclinicdemo.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
 }
--- src/main/java/com/example/petclinicdemo/repository/CustomerRepository.java
+++ src/main/java/com/example/petclinicdemo/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.example.petclinicdemo.repository;
 
 import com.example.petclinicdemo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
 }
