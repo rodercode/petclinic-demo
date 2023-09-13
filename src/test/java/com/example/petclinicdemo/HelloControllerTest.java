a85ca1ea7846174307176c4c753984b19ad7315a
rodercode <rodersnow@gmail.com>
2023-09-13 14:13:45 +0200

    commit

--- src/test/java/com/example/petclinicdemo/HelloControllerTest.java
+++ src/test/java/com/example/petclinicdemo/HelloControllerTest.java
@@ -1,7 +1,14 @@
 package com.example.petclinicdemo;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 // Testing!
 class HelloControllerTest {
+    @Test
+    void helloWorld() {
+        HelloController helloController = new HelloController();
+        assertEquals("Hello, World!", helloController.helloWorld());
+    }
 
 }
